17f4996d33c70da7fb21a713ec4065d2a1425881
HasbersHz <98599824+HasbersHz@users.noreply.github.com>
2022-05-27 17:41:33 +0500

    Update play_wav.py

--- play_wav.py
+++ play_wav.py
@@ -6,5 +6,9 @@ def play_wav(wavfile):
     AudioPlayer(wavfile).play(block=True)
 
 def saywav_to_file(saywav_result,wavfile):
-    import save_wav
-    save_wav.saywav_to_file(saywav_result,wavfile)
+    import base64
+
+    base64_message = saywav_result["wav_base64"].encode('utf-8')
+
+    with open(wavfile, 'wb') as file:
+        file.write(base64.decodebytes(base64_message))
