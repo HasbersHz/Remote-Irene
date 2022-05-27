8ae4b23ab94107c2d7d3d2738284d694034c9878
HasbersHz <98599824+HasbersHz@users.noreply.github.com>
2022-05-27 18:20:33 +0500

    Update run_remoteva_cmdline_min.py

--- run_remoteva_cmdline_min.py
+++ run_remoteva_cmdline_min.py
@@ -12,9 +12,7 @@ version="1.3"
 
 # ---------- main options -------------
 with open('options.json', 'r', encoding="utf-8") as f:
-    s = f.read(10000000)
-    f.close()
-saved_options = json.loads(s)
+    saved_options = json.loads(f.read(10000000))
 
 ttsFormat = saved_options["ttsFormat"] # "none" (TTS on server) or "saytxt" (text on client)
 ttsFormatList = ttsFormat.split(",")
@@ -23,26 +21,25 @@ baseUrl = saved_options["baseUrl"] # server with Irene WEB api
 
 # ----- main procedure -------
 if __name__ == "__main__":
-    print("Remote Irene (Command line variation min) v{0} started! ttsFormat={1}, baseUrl={2}".format(version,ttsFormat,baseUrl))
-    print("---------------------")
-    print("Введите команду для голосового помощника.")
-    print("Пример 'привет', 'брось кубик', 'exit'.")
-    while True:
-        cmd = input("VA CMD> ")
-        if cmd == "exit":
-            break
-        if cmd != "" and cmd != None:
+    print("Remote Irene (Command line variation min) v{0} started! ttsFormat={1}, baseUrl={2}".format(version,ttsFormat,baseUrl),
+          "---------------------",
+          "Введите команду для голосового помощника.",
+          "Пример 'привет', 'брось кубик', 'exit'.")
+    cmd = input("VA CMD> ")
+    while cmd not in ["exit", "q", "выход"]:
+        if cmd:
             try:
-                r = request(baseUrl+"sendTxtCmd", params={"cmd": cmd, "returnFormat": ttsFormat})
+                r = request(baseUrl + "sendTxtCmd", params={"cmd": cmd, "returnFormat": ttsFormat})
                 if r.status == 200:
-                    if r.body != "":
+                    if r.body:
                         res = json.loads(r.body)
-                        if res != None and res != "": # there is some response to play
+                        if res: # there is some response to play
                             if "restxt" in res.keys():
-                                print("ОТВЕТ: "+res["restxt"])
+                                print("OUTPUT: " + res["restxt"])
                 else:
                     print("Ошибка: статус не 200")
             except Exception as e:
-                print("Ошибка связи с сервером (вероятно) или обработки результата")
+                print("Ошибка связи с сервером (вероятно) или обработки результата", e)
+    cmd = input("VA CMD> ")
 
 
